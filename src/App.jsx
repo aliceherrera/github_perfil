bf4ed8a0fb4ee848b36b842464199b610ca27615
Alice Herrera <a.aliceherrera@gmail.com>
2024-05-22 16:05:15 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+import Perfil from "./components/Perfil";
+import Formulario from "./components/Formulario";
+import ReposList from "./components/reposList";
+
+function App() {
+  const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(true);
+  const [nomeUsuario, setNomeUsuario] = useState('')
+
+  return(
+    <>
+      <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)} placeholder="Digite o nome de usuário" />
+      
+      {nomeUsuario.length > 4 && (
+        <>
+          <Perfil nomeUsuario={nomeUsuario} />
+          <ReposList nomeUsuario={nomeUsuario} />
+        </>
+      )}
+      {/* {formularioEstaVisivel && (
+      <Formulario />)}
+      <button onClick={() => setFormularioEstaVisivel(!formularioEstaVisivel)} type="button">toggle form</button> */}
+    </>
+  )
+}
+
+export default App
